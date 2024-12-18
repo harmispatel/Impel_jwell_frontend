cf1b3e5310ed9f460ddfae17abb3cc7963e0f9e8
Harmis <harmis@Harmiss-Mac-mini-2.local>
2024-12-18 18:27:25 +0530

    design complete

--- src/pages/landing-page/LandingPage.js
+++ src/pages/landing-page/LandingPage.js
@@ -1,15 +1,25 @@
 import React from "react";
 import "./index.css";
-import bottom_banner from "./assets/bottom-banner.png";
-import insta_post from "./assets/insta-post.png";
-import trending_img_1 from "./assets/daily use bracelet for ladies.jpg";
-import trending_img_2 from "./assets/daily use bracelet.jpg";
-import trending_img_3 from "./assets/rings for daily use.jpg";
-import trending_img_4 from "./assets/bangles for daily use.jpg";
+import banner_1 from "./assets/Banner Images/Impel_Landing Page Banners_01.jpg";
+import banner_2 from "./assets/Banner Images/Impel_Landing Page Banners_02.jpg";
+import banner_3 from "./assets/Banner Images/Impel_Landing Page Banners_03.jpg";
+
+import product_1 from "./assets/3rd Section/Rings.jpg";
+import product_2 from "./assets/3rd Section/Necklace.jpg";
+import product_3 from "./assets/3rd Section/Earring.jpg";
+import product_4 from "./assets/3rd Section/Bracelet.jpg";
+
+import trending_img_1 from "./assets/Trending Products/Gold Bracelet.jpg";
+import trending_img_2 from "./assets/Trending Products/Necklace.jpg";
+import trending_img_3 from "./assets/Trending Products/Rings_1.jpg";
+import trending_img_4 from "./assets/Trending Products/Silver Bracelet.jpg";
+
 import love_img from "./assets/silver ring for women.jpg";
 import daily_wear from "./assets/daily wear necklace for women.jpg";
-import modern_bracelet from "./assets/modern bracelet designs.jpg";
-import minimalistic from "./assets/Minimalistic necklace for women.jpg";
+import modern_bracelet from "./assets/Bracelet_3.jpg";
+import Carousel from "react-bootstrap/Carousel";
+import { FaInstagram } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   const currentYear = new Date().getFullYear();
@@ -32,45 +42,68 @@ const LandingPage = () => {
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav mx-auto">
+            <ul class="navbar-nav mx-auto navbar-name-list">
               <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">
+                <Link class="nav-link nav-link-name" aria-current="page" to="#">
                   Home
-                </a>
+                </Link>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">
+                <Link class="nav-link nav-link-name" to="#">
                   About
-                </a>
+                </Link>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">
+                <Link class="nav-link nav-link-name" to="#">
                   Product
-                </a>
+                </Link>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">
+                <Link class="nav-link nav-link-name" to="#">
+                  Connectivity
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link nav-link-name" to="#">
                   Contact
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
         </div>
       </nav>
 
-      {/* Product Section Start */}
+      {/* Banner Section Start */}
+      <section>
+        <Carousel data-bs-theme="dark">
+          <Carousel.Item>
+            <img className="d-block w-100" src={banner_1} alt="First slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={banner_2} alt="First slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={banner_3} alt="First slide" />
+          </Carousel.Item>
+        </Carousel>
+      </section>
+      {/* Banner Section End */}
 
+      {/* Product Section Start */}
       <section className="product-section">
         <div className="row justify-content-center">
-          <div className="product-text text-center">
+          <div className="product-text product-info-text-name text-center">
             <h5>IMPEL JEWELRY</h5>
           </div>
-          <div className="col-md-6">
-            <div className="text-center">
+          <div className="col-md-8">
+            <div className="text-center product-info-text">
               <h4>
-                Lorem ipsum dolor sit amet consectetur adipiscing elit accumsan
-                per feugiat auctor tortor fringilla lectus, ligula habitant nunc
-                quis ultricies tristique.
+                Impel is a jewelry store in Ahmadabad offering minimalist
+                jewelry collection. We craft minimal jewelry for women designed
+                to bring timeless elegance and effortless style to everyday
+                moments. Founded by Mr. Bakul Shah in 1975, Impel Jewelry blends
+                elegance and simplicity. With over 50,000 designs, it’s become a
+                symbol of timeless, premium, minimal luxury for everyday wear.
               </h4>
             </div>
           </div>
@@ -78,22 +111,34 @@ const LandingPage = () => {
         <div className="row mt-3">
           <div className="col-md-3">
             <div className="product-card">
-              <img src={trending_img_1} alt="" className="w-100" />
+              <img src={product_1} alt="" className="w-100" />
+              <div className="product-card-name">
+                <h5>Rings</h5>
+              </div>
             </div>
           </div>
           <div className="col-md-3">
             <div className="product-card">
-              <img src={trending_img_2} alt="" className="w-100" />
+              <img src={product_2} alt="" className="w-100" />
+              <div className="product-card-name">
+                <h5>Necklace</h5>
+              </div>
             </div>
           </div>
           <div className="col-md-3">
             <div className="product-card">
-              <img src={minimalistic} alt="" className="w-100" />
+              <img src={product_3} alt="" className="w-100" />
+              <div className="product-card-name">
+                <h5>Earring</h5>
+              </div>
             </div>
           </div>
           <div className="col-md-3">
             <div className="product-card">
-              <img src={trending_img_4} alt="" className="w-100" />
+              <img src={product_4} alt="" className="w-100" />
+              <div className="product-card-name">
+                <h5>Bracelet</h5>
+              </div>
             </div>
           </div>
         </div>
@@ -105,18 +150,14 @@ const LandingPage = () => {
         <div className="row">
           <div className="col-md-6">
             <div className="banner-img">
-              <img
-                src={daily_wear}
-                alt=""
-                className="w-100"
-              />
+              <img src={daily_wear} alt="" className="w-100" />
             </div>
           </div>
           <div className="col-md-6 ">
             <div className="h-100 d-flex justify-content-center align-items-center">
               <div className="img-info text-center">
-                <h3>Lorem Ipsum</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipiscing</p>
+                <h3>Your Daily Dose of Grace —</h3>
+                <p>Minimal Jewelry Crafted to Add Grace to Every Day</p>
                 <button>See Now</button>
               </div>
             </div>
@@ -131,19 +172,15 @@ const LandingPage = () => {
           <div className="col-md-6">
             <div className="h-100 d-flex justify-content-center align-items-center">
               <div className="img-info text-center">
-                <h3>Lorem Ipsum</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipiscing</p>
+                <h3>Jewelry as Versatile as You —</h3>
+                <p>Minimal Jewelry for Every Style, Every Day, Every Moment.</p>
                 <button>See Now</button>
               </div>
             </div>
           </div>
           <div className="col-md-6">
             <div className="banner-img">
-              <img
-                src={modern_bracelet}
-                alt=""
-                className="w-100"
-              />
+              <img src={modern_bracelet} alt="" className="w-100" />
             </div>
           </div>
         </div>
@@ -160,13 +197,12 @@ const LandingPage = () => {
             <div class="product-card">
               <div class="badge">Hot</div>
               <div class="product-tumb">
-                <img src={trending_img_1} alt="" className="w-100"/>
+                <img src={trending_img_1} alt="" className="w-100" />
               </div>
               <div class="product-details">
                 <h4>
-                  <a href="">Women leather bag</a>
+                  <a href="">Gold Bracelet</a>
                 </h4>
-                <p className="text-success">₹ 100</p>
               </div>
             </div>
           </div>
@@ -174,13 +210,10 @@ const LandingPage = () => {
             <div class="product-card">
               <div class="badge">Hot</div>
               <div class="product-tumb">
-                <img src={trending_img_2} alt="" className="w-100"/>
+                <img src={trending_img_2} alt="" className="w-100" />
               </div>
               <div class="product-details">
-                <h4>
-                  <a href="">Women leather bag</a>
-                </h4>
-                <p className="text-success">₹ 100</p>
+                <h4>Necklace</h4>
               </div>
             </div>
           </div>
@@ -188,13 +221,12 @@ const LandingPage = () => {
             <div class="product-card">
               <div class="badge">Hot</div>
               <div class="product-tumb">
-                <img src={trending_img_3} alt="" className="w-100"/>
+                <img src={trending_img_3} alt="" className="w-100" />
               </div>
               <div class="product-details">
                 <h4>
-                  <a href="">Women leather bag</a>
+                  <a href="">Ring</a>
                 </h4>
-                <p className="text-success">₹ 100</p>
               </div>
             </div>
           </div>
@@ -202,13 +234,12 @@ const LandingPage = () => {
             <div class="product-card">
               <div class="badge">Hot</div>
               <div class="product-tumb">
-                <img src={trending_img_4} alt="" className="w-100"/>
+                <img src={trending_img_4} alt="" className="w-100" />
               </div>
               <div class="product-details">
                 <h4>
-                  <a href="">Women leather bag</a>
+                  <a href="">Silver Bracelet</a>
                 </h4>
-                <p className="text-success">₹ 100</p>
               </div>
             </div>
           </div>
@@ -222,20 +253,18 @@ const LandingPage = () => {
           <div className="col-md-6">
             <div className="h-100 d-flex align-items-center">
               <div className="img-info">
-                <h2>It started with love</h2>
+                <h2>It Started With Love</h2>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipiscing elit felis
-                  faucibus, parturient fermentum elementum erat litora habitasse
-                  eget conubia leo, vel scelerisque montes fames dapibus
-                  praesent cubilia ridiculus.
+                  Our commitment to quality begins with meticulously inspected
+                  and precisely measured raw materials. Guided by a team of the
+                  most skilled designers in the market, we bring stunning
+                  jewelry designs to life.
                 </p>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipiscing elit felis
-                  faucibus, parturient fermentum elementum erat litora habitasse
-                  eget conubia leo, vel scelerisque montes fames dapibus
-                  praesent cubilia ridiculus.
+                  This expertise in jewelry casting is seamlessly paired with
+                  our renowned in-house design capabilities, delivering
+                  exceptional creations for our customers.
                 </p>
-                <button>See Now</button>
               </div>
             </div>
           </div>
@@ -252,6 +281,7 @@ const LandingPage = () => {
       <section className="insta-section mt-4">
         <div className="row justify-content-center">
           <div className="product-text text-center mt-4">
+            <FaInstagram className="instagram-icon" />
             <h4>impel_store instagram</h4>
           </div>
           <div className="row mt-3 mb-5">
@@ -274,9 +304,24 @@ const LandingPage = () => {
 
       {/* Latest News Section Start */}
       <section className="latest-news mt-5 mb-5">
-        <div className="row">
+        {/* <div className="row">
           <div className="newsletter text-center">
             <h3>Latest From Impel!</h3>
+          </div>
+        </div> */}
+        <div class="row justify-content-center">
+          <div class="col-md-8 col-lg-6">
+            <div class="latest_product_info newsletter">
+              <h3>latest From Impel!</h3>
+              <div class="form-group">
+                <input
+                  type="email"
+                  class="form-control"
+                  placeholder="Email address"
+                />
+                <button class="btn inq_btn">inquiry</button>
+              </div>
+            </div>
           </div>
         </div>
       </section>
